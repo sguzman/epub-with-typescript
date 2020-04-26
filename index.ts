4de47f6a2316fe9949c4bb92e7d30b2e3546ea75
Salvador Guzman Jr <guzmansalv@gmail.com>
2020-04-25 20:16:25 -0700

    working metadata extraction

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
-import 'epub';
-import 'cheerio';
+import * as epubAPI from 'epub';
+import { cheerio } from 'cheerio';
 
 export function main() {
-    console.log('Hello')
+    const epubPath: string = 'C:\\Users\\sguzman\\Documents\\Software\\Web\\epub-with-typescript\\Pragmatic Programmer, The_ From - Pragmatic Programmer, The From .epub'
+
+    const epub = new epubAPI(epubPath);
+    epub.on('end', function () {
+        console.log(epub.metadata);
+        epub.flow.forEach(function (chapter) {
+            console.log(chapter);
+        })
+    });
+
+    epub.parse();
 }
 
 main();
